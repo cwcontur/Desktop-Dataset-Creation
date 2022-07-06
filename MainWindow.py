a43cedc0525b1e20a5325e62f7c2bb75e8353454
Connor Contursi <cwcontur@uncg.edu>
2022-07-06 16:51:16 -0400

    more & more & more + organization

--- MainWindow.py
+++ MainWindow.py
@@ -1,3 +1,17 @@
+# A dataset creation tool.
+#
+#   ,-.       _,---._ __  / \
+#  /  )    .-'       `./ /   \
+# (  (   ,'            `/    /|
+#  \  `-"             \'\   / |
+#   `.              ,  \ \ /  |
+#    /`.          ,'-`----Y   |
+#   (            ;        |   '
+#   |  ,-.    ,-'  Connor |  /
+#   |  | (   |   Contursi | /
+#   )  |  \  `.___________|/
+#   `--'   `--'
+
 from copyreg import pickle
 from logging import root
 import sys
@@ -37,29 +51,32 @@ class MainWindow(QMainWindow):
         self.initUI()
         
     def initUI(self):
-        # Loads splash screen that stays persistent till the program is finished starting up
-        pic = QPixmap("cookies_jar.png")
-        pix = pic.scaled(300, 300, QtCore.Qt.KeepAspectRatio) # Scales the splash screen down to 300px * 300px
-        splash = QSplashScreen(pix)
-        splash.show()        
-        
-        
-        
         global root_directory
         global image_directory
+        # Source path for application
         root_directory = os.path.dirname(os.path.abspath("__file__"))
-        print(root_directory)
-        
+        # Source path for image files
         image_directory = os.path.join(root_directory, "image_data\\")
-        print(image_directory)
+        # Source path for GUI icons
+        iconPath = os.path.join(root_directory, "icons\\")
         
+        # Loads splash screen that stays persistent till the program is finished starting up
+        pic = QPixmap(os.path.join(iconPath, "cookies_jar.png"))
+        pix = pic.scaled(300, 300, QtCore.Qt.KeepAspectRatio) # Scales the splash screen down to 300px * 300px
+        splash = QSplashScreen(pix)
+        splash.show()                
+        
+        # Creates global variables that get the number of images and keep track of which file in the directory is being accessed
         global images
         global progression
         images = len(os.listdir(image_directory)) # Counts the number of files in the image directory
         progression = 0
         
+        # * ----------------
+        # * collects inputs for each image [globally] and stores them in a 2D array
         global inputs
         inputs = [6, images]
+        # * ----------------
         
         # Array of zeroes with a place for each image
         global mylist
@@ -83,54 +100,67 @@ class MainWindow(QMainWindow):
                 images += 1  # ! Count of the pictures found
 
         mylist = os.listdir(image_directory)
-        
         self.image_files = mylist
         
-
-
+        windowIcon = os.path.join(iconPath, 'cookie.png')
+        iconWin = QtGui.QIcon(windowIcon)
+        self.setWindowIcon(iconWin)
         self.setWindowTitle("Dataset Label Creator")
         self.setMinimumSize(QSize(800, 535))
         self.setMaximumSize(QSize(1500, 1000))
-        self.setWindowIcon(QIcon("cookie.png"))
         
+        # ? Creates QLabel for the images from the dataset which will be displayed
         self.image = QLabel("Data")
-
         self.ontoNext()
         
         # ! -----------------------------------------------------------------------------
         # ! Creates all of the toggle buttons based on input [keyboard shortcuts included]
-        self.she = AnimatedToggle(checked_color="#e6382c", pulse_checked_color="#bfbfbf")
+        # ? =============================================================================
+        self.she = AnimatedToggle(checked_color="#e6382c", pulse_checked_color="#bfbfbf") # Red, Grey
         self.she.setChecked(False)
         self.she.setShortcut(QKeySequence("Left"))
         self.she.setStatusTip("Use 'Left Arrow' to toggle")
-        self.she.toggled.connect(self.test)
-        self.her = AnimatedToggle(checked_color="#e6732c", pulse_checked_color="#bfbfbf")
+        self.she.toggled.connect(self.she)
+        #   --------------------------------------
+        self.her = AnimatedToggle(checked_color="#e6732c", pulse_checked_color="#bfbfbf") # Orange, Grey
         self.her.setChecked(False)
         self.her.setShortcut(QKeySequence("Shift+Left"))
         self.her.setStatusTip("Use 'Shift+Left Arrow' to toggle")
-        self.he = AnimatedToggle(checked_color="#ebd82f", pulse_checked_color="#bfbfbf")
+        self.her.toggled.connect(self.her)
+        # ? =============================================================================
+        self.he = AnimatedToggle(checked_color="#ebd82f", pulse_checked_color="#bfbfbf") # Yellow, Grey
         self.he.setChecked(False)
         self.he.setShortcut(QKeySequence("Down"))
         self.he.setStatusTip("Use 'Down Arrow' to toggle")
-        self.him = AnimatedToggle(checked_color="#3ac25f", pulse_checked_color="#bfbfbf")
+        self.he.toggled.connect(self.he)
+        #   --------------------------------------       
+        self.him = AnimatedToggle(checked_color="#3ac25f", pulse_checked_color="#bfbfbf") # Green, Grey
         self.him.setChecked(False)
         self.him.setShortcut(QKeySequence("Shift+Down"))
         self.him.setStatusTip("Use 'Sift+Down Arrow' to toggle")
-        self.they = AnimatedToggle(checked_color="#2f5eeb", pulse_checked_color="#bfbfbf")
+        self.him.toggled.connect(self.him)
+        # ? =============================================================================
+        self.they = AnimatedToggle(checked_color="#2f5eeb", pulse_checked_color="#bfbfbf") # Blue, Grey
         self.they.setChecked(False) 
         self.they.setShortcut(QKeySequence("Right"))
         self.they.setStatusTip("Use 'Right Arrow' to toggle")
-        self.them = AnimatedToggle(checked_color="#612feb", pulse_checked_color="#bfbfbf")
+        self.they.toggled.connect(self.they)
+        #   --------------------------------------        
+        self.them = AnimatedToggle(checked_color="#612feb", pulse_checked_color="#bfbfbf") # Purple, Grey
         self.them.setChecked(False) 
         self.them.setShortcut(QKeySequence("Shift+Right"))
         self.them.setStatusTip("Use 'Shift+Down Arrow' to toggle")
+        self.them.toggled.connect(self.them)
+        # ? =============================================================================       
         # ! -----------------------------------------------------------------------------
         
         # ? Submits button states and moves onto the next image for labeling 
         self.next = QPushButton('Submit', self)
         self.next.clicked.connect(self.ontoNext)
         
-        # * Creates the progress bar and limits it for the number of images that will be processed
+        # * Creates the progress bar and limits it for the number of images that will be processed;
+        # * Additionally, it doesn't keep track via percentage, it displays the number of completed,
+        # * labels compared to the total number of images that need labels
         self.progressBar = QProgressBar(self)
         self.progressBar.setGeometry(0, 0, 300, 25)
         self.progressBar.setAlignment(Qt.AlignCenter)
@@ -143,7 +173,7 @@ class MainWindow(QMainWindow):
         buttons_layout = QVBoxLayout()
         submit_layout = QVBoxLayout()
         
-
+        # ? Buttons added to the GUI layout to be displayed
         buttons_layout.addWidget(self.progressBar)
         buttons_layout.addWidget(self.she)
         buttons_layout.addWidget(self.her)
@@ -152,14 +182,15 @@ class MainWindow(QMainWindow):
         buttons_layout.addWidget(self.they)
         buttons_layout.addWidget(self.them)
         buttons_layout.addWidget(self.next)
-        
-        
+              
+        # * Adds status bar at bottom for tips
         self.setStatusBar(QStatusBar(self))
         
         base_layout.addLayout(buttons_layout)
         widget = QWidget()
         widget.setLayout(base_layout)
         self.setCentralWidget(widget)
+        
         splash.finish(self) #Closes splash screen after successful launch
         self.show()
         
@@ -169,12 +200,47 @@ class MainWindow(QMainWindow):
         file = self.image_files[self.progress]
         # Increments progress bar
         
-    def test(self, s):
+# ? =============================
+# ! She/Her             
+    def she(self, s):
         if(s):
-            print("dumbass")
+            print("dumbass") #prints if true
         else:
-            print("idot")
-    
+            print("idot") #prints if false
+#   -----------------------------            
+    def her(self, s):
+        if(s):
+            print("dumbass") #prints if true
+        else:
+            print("idot") #prints if false
+# ? =============================     
+# ! He/Him            
+    def he(self, s):
+        if(s):
+            print("dumbass") #prints if true
+        else:
+            print("idot") #prints if false
+#   -----------------------------            
+    def him(self, s):
+        if(s):
+            print("dumbass") #prints if true
+        else:
+            print("idot") #prints if false
+# ? =============================      
+# ! They/Them            
+    def they(self, s):
+        if(s):
+            print("dumbass") #prints if true
+        else:
+            print("idot") #prints if false
+#   -----------------------------            
+    def them(self, s):
+        if(s):
+            print("dumbass") #prints if true
+        else:
+            print("idot") #prints if false    
+# ? =============================            
+
 # * -----------------------------        
     def ontoNext(self):
         global progression
