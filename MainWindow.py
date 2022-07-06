fed11e706bcc3a63028f63bd69f4ff527781f48c
Connor Contursi <cwcontur@uncg.edu>
2022-07-06 16:24:12 -0400

    More updates

--- MainWindow.py
+++ MainWindow.py
@@ -16,6 +16,11 @@ from matplotlib import image
 from pydantic import root_validator
 from qtwidgets import Toggle, AnimatedToggle
 
+from PyQt5.QtWidgets import * 
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import * 
+from PyQt5.QtCore import * 
+import sys
 # global variables for the current image number [progress], and the number of images [images]
 
 class MainWindow(QMainWindow):
@@ -25,28 +30,16 @@ class MainWindow(QMainWindow):
     
     images = 1000
     progress = 0
+    
+    inputs = [6, images]
     def __init__(self):
         super().__init__()
         self.initUI()
         
     def initUI(self):
-        # pagelayout = QVBoxLayout()
-        # self.stackedWindow = QStackedLayout()
-        
-        # splashImage = QLabel("Splush")
-        # splashImage.setPixmap(QPixmap("cookie_jar.png"))
-        # pagelayout.addWidget(splashImage)
-        # pagelayout.addLayout(self.stackedWindow)
-        # splunk = QWidget()
-        # splunk.setLayout(pagelayout)
-        # self.setCentralWidget(splunk)        
-        # pagelayout.addLayout(self.stackedWindow)
-        # splash = QIcon("cookie_jar.png")
-        # pagelayout.addWidget(splash)
-        # pagelayout.setCentralWidget(splash)
-        # self.setLayout(pagelayout)
+        # Loads splash screen that stays persistent till the program is finished starting up
         pic = QPixmap("cookies_jar.png")
-        pix = pic.scaled(300, 300, QtCore.Qt.KeepAspectRatio)
+        pix = pic.scaled(300, 300, QtCore.Qt.KeepAspectRatio) # Scales the splash screen down to 300px * 300px
         splash = QSplashScreen(pix)
         splash.show()        
         
@@ -64,6 +57,9 @@ class MainWindow(QMainWindow):
         global progression
         images = len(os.listdir(image_directory)) # Counts the number of files in the image directory
         progression = 0
+        
+        global inputs
+        inputs = [6, images]
         
         # Array of zeroes with a place for each image
         global mylist
@@ -95,65 +91,76 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Dataset Label Creator")
         self.setMinimumSize(QSize(800, 535))
         self.setMaximumSize(QSize(1500, 1000))
-        
-        self.setWindowIcon(QIcon("cookie--pencil.png"))
+        self.setWindowIcon(QIcon("cookie.png"))
         
         self.image = QLabel("Data")
-        # self.image.setPixmap(QPixmap(self.image_files[progress]))
-        # self.image.setScaledContents(True)
+
         self.ontoNext()
         
-        # image = QPixmap(self.image_files[progress])
-        # self.label.setPixmap(image)
-        
         # ! -----------------------------------------------------------------------------
-        #Creates all of the toggle buttons based on input
+        # ! Creates all of the toggle buttons based on input [keyboard shortcuts included]
         self.she = AnimatedToggle(checked_color="#e6382c", pulse_checked_color="#bfbfbf")
-        self.she.setChecked(True)
-        self.her = AnimatedToggle(checked_color="#e6572c", pulse_checked_color="#bfbfbf")
-        self.her.setChecked(True)
-        
+        self.she.setChecked(False)
+        self.she.setShortcut(QKeySequence("Left"))
+        self.she.setStatusTip("Use 'Left Arrow' to toggle")
+        self.she.toggled.connect(self.test)
+        self.her = AnimatedToggle(checked_color="#e6732c", pulse_checked_color="#bfbfbf")
+        self.her.setChecked(False)
+        self.her.setShortcut(QKeySequence("Shift+Left"))
+        self.her.setStatusTip("Use 'Shift+Left Arrow' to toggle")
         self.he = AnimatedToggle(checked_color="#ebd82f", pulse_checked_color="#bfbfbf")
-        self.he.setChecked(True)
-        self.him = AnimatedToggle(checked_color="#a9eb2f", pulse_checked_color="#bfbfbf")
-        self.him.setChecked(True)
-        
+        self.he.setChecked(False)
+        self.he.setShortcut(QKeySequence("Down"))
+        self.he.setStatusTip("Use 'Down Arrow' to toggle")
+        self.him = AnimatedToggle(checked_color="#3ac25f", pulse_checked_color="#bfbfbf")
+        self.him.setChecked(False)
+        self.him.setShortcut(QKeySequence("Shift+Down"))
+        self.him.setStatusTip("Use 'Sift+Down Arrow' to toggle")
         self.they = AnimatedToggle(checked_color="#2f5eeb", pulse_checked_color="#bfbfbf")
-        self.they.setChecked(True) 
+        self.they.setChecked(False) 
+        self.they.setShortcut(QKeySequence("Right"))
+        self.they.setStatusTip("Use 'Right Arrow' to toggle")
         self.them = AnimatedToggle(checked_color="#612feb", pulse_checked_color="#bfbfbf")
-        self.them.setChecked(True) 
+        self.them.setChecked(False) 
+        self.them.setShortcut(QKeySequence("Shift+Right"))
+        self.them.setStatusTip("Use 'Shift+Down Arrow' to toggle")
         # ! -----------------------------------------------------------------------------
         
-        # Submits button states and moves onto the next image for labeling 
+        # ? Submits button states and moves onto the next image for labeling 
         self.next = QPushButton('Submit', self)
         self.next.clicked.connect(self.ontoNext)
         
-        # Creates the progress bar and limits it for the number of images that will be processed
-
-        self.progress_Bar = QProgressBar(self)
-        self.progress_Bar.setGeometry(0, 0, 300, 25)
-        self.progress_Bar.setMaximum(images) 
-
-        # All layouts needed to be arranged for the GUI
+        # * Creates the progress bar and limits it for the number of images that will be processed
+        self.progressBar = QProgressBar(self)
+        self.progressBar.setGeometry(0, 0, 300, 25)
+        self.progressBar.setAlignment(Qt.AlignCenter)
+        self.progressBar.setFormat("%v/%m")
+        self.progressBar.setMaximum(images) 
+        
+        # ! All layouts needed to be arranged for the GUI
         base_layout = QHBoxLayout()
         image_layout = QVBoxLayout()
         buttons_layout = QVBoxLayout()
         submit_layout = QVBoxLayout()
         
-        buttons_layout.addWidget(self.image)
-        buttons_layout.addWidget(self.progress_Bar)
+
+        buttons_layout.addWidget(self.progressBar)
         buttons_layout.addWidget(self.she)
+        buttons_layout.addWidget(self.her)
         buttons_layout.addWidget(self.he)
+        buttons_layout.addWidget(self.him)
         buttons_layout.addWidget(self.they)
+        buttons_layout.addWidget(self.them)
         buttons_layout.addWidget(self.next)
         
         
+        self.setStatusBar(QStatusBar(self))
         
         base_layout.addLayout(buttons_layout)
         widget = QWidget()
         widget.setLayout(base_layout)
         self.setCentralWidget(widget)
-        splash.finish(self)
+        splash.finish(self) #Closes splash screen after successful launch
         self.show()
         
 # * -----------------------------    
@@ -162,15 +169,20 @@ class MainWindow(QMainWindow):
         file = self.image_files[self.progress]
         # Increments progress bar
         
-
+    def test(self, s):
+        if(s):
+            print("dumbass")
+        else:
+            print("idot")
     
 # * -----------------------------        
     def ontoNext(self):
         global progression
         progression += 1
         global images
+        # print(self.she.isEnabled())            
         # if progression <= images:
-        #     self.progress_Bar.setValue(progression)
+        #     self.progressBar.setValue(progression)
         # self.updateData()
         
 # * ----------------------------- 
